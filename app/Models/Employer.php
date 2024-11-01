ffd6f36495550b14091834cb11eb7e9e5b1788e5
appserviceprovider <roman.shaon@gmail.com>
2024-11-02 00:52:47 +0600

    employer hasMany relation

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Employer extends Model
 {
     use HasFactory;
+    protected $fillable= ["name"] ;
+
+    public function jobs(){
+        return $this->hasMany(JobListing::class);
+    }
 }
